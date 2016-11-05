50737e72f6fb9b9d517ce3a7850773bc1e55142d
minemidnight <minemidnight@users.noreply.github.com>
2016-11-05 09:13:27 -0400

    Updated polls.js
    Small update

--- modules/polls.js
+++ modules/polls.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js"),
+      duration = require("duration-js"),
       Oxyl = require("../oxyl.js");
 const bot = Oxyl.bot;
 
-var newPoll = (message, timeMs, topic, callback) => {
+var newPoll = (message, timespan, topic, callback) => {
     var channel = message.channel;
     var author = message.author;
+    var durationParsed = new Duration(duration);
+    var timeMs = duration.minutes()*1000;
     var pollResult, noVotes, yesVotes;
     
     var mainMsg = `:pushpin: New poll created by **${author.username}**` +
@@ -25,10 +28,10 @@ var newPoll = (message, timeMs, topic, callback) => {
             pollResult = `\n\n:gem: **Results:**` +
                `\n        **╠** :x: (no): ${noVotes}` +
                `\n        **╚** :white_check_mark: (yes): ${yesVotes}`;
-            }, timeMs);
+            });
 
             msg.edit(`${mainMsg}${timeMsg}:no_entry_sign: 0m0s${pollResult}`);
-        });  
+        }, timeMs);  
     });
 }
 
