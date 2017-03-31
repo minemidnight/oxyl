2a608857af297a00bc31044f851454c9c32529dc
minemidnight <minemidnightmc@gmail.com>
2017-03-31 17:23:59 -0400

    clean up repo

--- commands/default/support.js
+++
@@ -1,5 +0,0 @@
-exports.cmd = new Oxyl.Command("support", async message => `Support Guild: https://discord.gg/9wkTDcE\nInvite Link: ${framework.config.inviteLink}`, {
-	type: "default",
-	description: "Get invite link to the support guild, aswell as Oxyl's invite link",
-	aliases: ["invite"]
-});
