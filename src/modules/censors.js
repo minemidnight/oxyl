83034ff9a46ffb1c69a0d9f2a4afed2e42ef1903
minemidnight <minemidnightmc@gmail.com>
2017-06-12 14:49:30 -0400

    debug and stuffs

--- src/modules/censors.js
+++ src/modules/censors.js
@@ -4,38 +4,38 @@ module.exports = async message => {
 	if(!censors) return;
 
 	for(let censor in Array.from(censors.values())) {
-		if(message.content.match(new RegExp(censor.regex, "i"))) {
-			message.delete();
-			message.channel.createMessage(__("modules.censor", message.channel.guild, { mention: message.author.mention }));
+		console.log(censor);
+		if(!message.content.match(new RegExp(censor.regex, "i"))) return;
+		message.delete();
+		message.channel.createMessage(__("modules.censor", message.channel.guild, { mention: message.author.mention }));
 
-			if(censor.action === "warn") {
-				await bot.utils.warnMember(message.member, null, "Said a censored phrase");
-			} else if(censor.action === "kick") {
-				let channel = await modLog.channel(message.channel.guild);
-				if(channel) {
-					modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
-				}
-
-				await message.member.kick("Said a censored phrase");
-				modLog.create(message.channel.guild, "kick", message.author);
-			} else if(censor.action === "softban") {
-				let channel = await modLog.channel(message.channel.guild);
-				if(channel) {
-					modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
-				}
-
-				await message.member.ban(7, "Said a censored phrase");
-				await message.member.unban("Softban");
-			} else if(censor.action === "ban") {
-				let channel = await modLog.channel(message.channel.guild);
-				if(channel) {
-					modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
-				}
-
-				await message.member.ban(7, "Said a censored phrase");
+		if(censor.action === "warn") {
+			await bot.utils.warnMember(message.member, null, "Said a censored phrase");
+		} else if(censor.action === "kick") {
+			let channel = await modLog.channel(message.channel.guild);
+			if(channel) {
+				modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
 			}
 
-			break;
+			await message.member.kick("Said a censored phrase");
+			modLog.create(message.channel.guild, "kick", message.author);
+		} else if(censor.action === "softban") {
+			let channel = await modLog.channel(message.channel.guild);
+			if(channel) {
+				modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
+			}
+
+			await message.member.ban(7, "Said a censored phrase");
+			await message.member.unban("Softban");
+		} else if(censor.action === "ban") {
+			let channel = await modLog.channel(message.channel.guild);
+			if(channel) {
+				modLog.presetReasons[message.channel.guild.id] = { mod: bot.user, reason: "Said a censored phrase" };
+			}
+
+			await message.member.ban(7, "Said a censored phrase");
 		}
+
+		break;
 	}
 };
