aabc7e6944f2a40f5ceec8058c438dc0c695834e
minemidnight <minemidnightmc@gmail.com>
2017-06-12 16:11:04 -0400

    turn off pool for master

--- src/utils/rethink.js
+++ src/utils/rethink.js
@@ -12,6 +12,7 @@ module.exports = {
 		let connectionInfo = privateConfig.database;
 		connectionInfo.silent = true;
 		connectionInfo.db = dbName;
+		connectionInfo.pool = false;
 		const r = rethinkdbdash(connectionInfo); // eslint-disable-line id-length
 
 		let dbs = await r.dbList().run();
@@ -35,7 +36,7 @@ module.exports = {
 		}
 		await Promise.all(tableWait);
 		console.startup(`RethinkDB initated on master`);
-		await r.poolMaster.drain();
+		await r.close();
 		return true;
 	},
 	connect: async () => {
