545e1ce088bb0cc89701c4bd38eb9cba5cebf356
minemidnight <minemidnightmc@gmail.com>
2017-06-12 14:27:59 -0400

    fix permission check

--- src/listeners/on/channelCreate.js
+++ src/listeners/on/channelCreate.js
@@ -1,11 +1,8 @@
 module.exports = async channel => {
-	console.log(channel);
 	if(channel.type !== 0) return;
 
-	console.log("is text");
-	let rolePerms = channel.guild.members.get(bot.user.id).permission.has("manageChannel");
-	if(!rolePerms) return;
-	console.log("has role perms");
+	let manageChannels = channel.guild.members.get(bot.user.id).permission.has("manageChannels");
+	if(!manageChannels) return;
 
 	let mutedRole = channel.guild.roles.find(role => role.name.toLowerCase() === __("words.muted", channel.guild));
 	if(mutedRole) bot.editChannelPermission(channel.id, mutedRole.id, 0, 2048, "role", "Set Muted Role Permissions");
