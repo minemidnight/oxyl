ad723fce1b880b5954f8358cd523ed4a93061b5a
minemidnight <minemidnightmc@gmail.com>
2017-09-02 23:41:33 -0400

    fix bug

--- src/bot/modules/audio/main.js
+++ src/bot/modules/audio/main.js
@@ -2,18 +2,21 @@ const superagent = require("superagent");
 const playlist = require(`${__dirname}/playlist.js`);
 const search = require(`${__dirname}/search.js`);
 
-module.exports = async query => {
+module.exports = async (query, i = 0) => {
 	if(playlistRegex.test(query)) return playlist(query.match(playlistRegex)[1]);
 
 	try {
-		let { body: [data] } = await superagent.get(bot.config.lavalink.url)
+		var { body } = await superagent.get(bot.config.lavalink.url)
 			.set("Authorization", bot.config.lavalink.auth)
 			.query({ identifier: query });
-
-		return Object.assign(data.info, { track: data.track });
 	} catch(err) {
-		return module.exports(search(query));
+		if(i) throw err;
+		return module.exports(search(query), i + 1);
 	}
+
+	if(body && Array.isArray(body)) var data = [body];
+	else throw new Error("No track resolved");
+	return Object.assign(data.info, { track: data.track });
 };
 
 const playlistRegex = /^(?:http|https|)(?::\/\/|)(?:www.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{12,})[a-z0-9;:@#?&%=+\/\$_.-]*/; // eslint-disable-line max-len
