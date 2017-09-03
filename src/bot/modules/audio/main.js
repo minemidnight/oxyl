adedddec4a1d8fe0d88d8422489b0e0b575053e0
minemidnight <minemidnightmc@gmail.com>
2017-09-02 23:48:18 -0400

    fix searching

--- src/bot/modules/audio/main.js
+++ src/bot/modules/audio/main.js
@@ -2,19 +2,15 @@ const superagent = require("superagent");
 const playlist = require(`${__dirname}/playlist.js`);
 const search = require(`${__dirname}/search.js`);
 
-module.exports = async (query, i = 0) => {
+module.exports = async (query, search = false) => {
 	if(playlistRegex.test(query)) return playlist(query.match(playlistRegex)[1]);
 
-	try {
-		var { body } = await superagent.get(bot.config.lavalink.url)
-			.set("Authorization", bot.config.lavalink.auth)
-			.query({ identifier: query });
-	} catch(err) {
-		if(i) throw err;
-		return module.exports(search(query), i + 1);
-	}
+	let { body } = await superagent.get(bot.config.lavalink.url)
+		.set("Authorization", bot.config.lavalink.auth)
+		.query({ identifier: query });
 
 	if(body && Array.isArray(body) && body.length) var data = [body];
+	else if(!search) return module.exports(search(query), true);
 	else throw new Error("No track resolved");
 
 	data.info.track = data.track;
