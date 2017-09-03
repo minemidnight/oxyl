931beaf36ce10fd83a63307cfa941b1d09217bd2
minemidnight <minemidnightmc@gmail.com>
2017-09-03 01:12:49 -0400

    fix emoji search again

--- src/website/routes/emojis.js
+++ src/website/routes/emojis.js
@@ -3,7 +3,7 @@ const router = module.exports = require("express").Router(); // eslint-disable-l
 let emojis = {};
 router.get("/", async (req, res) => {
 	let displayEmojis = emojis;
-	if(req.query.search) displayEmojis = displayEmojis.filter(emoji => ~emojis.name.indexOf(req.query.search));
+	if(req.query.search) displayEmojis = displayEmojis.filter(emoji => ~emoji.name.indexOf(req.query.search));
 
 	let page = req.query.page ? parseInt(req.query.page) : 1;
 	let totalPages = Math.ceil(displayEmojis.length / 1500);
