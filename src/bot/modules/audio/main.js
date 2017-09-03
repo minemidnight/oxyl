64ce1fdeaa56f827952f21e2ed07e30bcfde9304
minemidnight <minemidnightmc@gmail.com>
2017-09-02 23:54:37 -0400

    fix info?

--- src/bot/modules/audio/main.js
+++ src/bot/modules/audio/main.js
@@ -13,8 +13,7 @@ module.exports = async (query, searching = false) => {
 	else if(body && Array.isArray(body) && !body.length && !searching) return module.exports(await search(query), true);
 	else throw new Error("No track resolved");
 
-	data.info.track = data.track;
-	return data.info;
+	return Object.assign(data.info, { track: data.track });
 };
 
 const playlistRegex = /^(?:http|https|)(?::\/\/|)(?:www.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{12,})[a-z0-9;:@#?&%=+\/\$_.-]*/; // eslint-disable-line max-len
