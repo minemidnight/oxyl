45f8ce027dd4cf520e8d4579eed401f05629e6c6
minemidnight <minemidnightmc@gmail.com>
2017-09-24 18:16:01 -0400

    byemom test thing

---
+++ src/bot/modules/images/byemom.js
@@ -0,0 +1,14 @@
+const Jimp = require("jimp");
+
+async function generate(text) {
+	const textImage = await new Jimp(500, 500);
+	const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+	textImage.print(font, 0, 250, text);
+	textImage.rotate(35);
+
+	const image = await Jimp.read(`${__dirname}/byemom.png`);
+	image.composite(textImage, 200, 200);
+	image.getBase64(Jimp.MIME_PNG, console.log);
+}
+
+generate(process.env.TEXT);
---
+++ src/bot/modules/images/main.js
@@ -0,0 +1,19 @@
+const exec = Promise.promisify(require("child_process").exec);
+const path = require("path");
+
+module.exports = {
+	byeMom: async text => {
+		const { stdout } = await exec(`node ${path.resolve(`${__dirname}/byemom.js`)}`, {
+			env: { TEXT: text },
+			maxBuffer: Infinity
+		});
+
+		return module.exports.imageFromURI(stdout);
+	},
+	imageFromURI: data => {
+		let [, ext, buffer] = data.match(/^data:image\/([A-Za-z-+\/]+);base64,(.+)$/);
+		buffer = Buffer.from(buffer, "base64");
+
+		return { buffer, ext };
+	}
+};
