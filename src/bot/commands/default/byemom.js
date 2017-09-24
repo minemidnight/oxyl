6c5b82c8d8155e05b19e3633e7b04289d60eb773
minemidnight <minemidnightmc@gmail.com>
2017-09-24 18:49:04 -0400

    add bye mom command

---
+++ src/bot/commands/default/byemom.js
@@ -0,0 +1,12 @@
+const images = require("../../modules/images/main");
+module.exports = {
+	process: async message => {
+		let { buffer, ext } = await images(message.args[0]);
+		return ["", {
+			file: buffer,
+			name: `byemom.${ext}`
+		}];
+	},
+	description: "Create a \"ok bye mom\" meme",
+	args: [{ type: "text" }]
+};
