6b43b2139be687ddd94991dba68820226d3b3165
minemidnight <minemidnightmc@gmail.com>
2017-09-24 18:54:43 -0400

    fix byemom again

--- src/bot/modules/images/byemom.js
+++ src/bot/modules/images/byemom.js
@@ -4,10 +4,10 @@ async function generate(text) {
 	const textImage = await new Jimp(500, 500);
 	const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
 	textImage.print(font, 0, 250, text);
-	textImage.rotate(-26, false);
+	textImage.rotate(-25, false);
 
 	const image = await Jimp.read(`${__dirname}/byemom.png`);
-	image.composite(textImage, 235, 150);
+	image.composite(textImage, 325, 235);
 	image.getBase64(Jimp.MIME_PNG, (err, data) => {
 		if(err) throw err;
 		process.stdout.write(data);
