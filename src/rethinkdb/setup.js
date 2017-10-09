e930bc38105fe2d10e80a92c15db5ea1eedf92d2
minemidnight <minemidnightmc@gmail.com>
2017-10-09 17:54:16 -0400

    Fix some rethink stuffs

--- src/rethinkdb/setup.js
+++ src/rethinkdb/setup.js
@@ -1,15 +1,19 @@
+const config = require("../../config");
 const { readdir } = require("fs");
 const { promisify } = require("util");
 const readdirAsync = promisify(readdir);
 const path = require("path");
 
 module.exports = async r => { // eslint-disable-line id-length
+	const dbList = await r.dbList().run();
+	if(!~dbList.indexOf(config.database.db)) await r.dbCreate(config.database.db).run();
+
 	const tableList = await r.tableList().run();
 	const tablesExpected = (await readdirAsync(path.resolve(__dirname, "tables")))
 		.map(file => require(path.resolve(__dirname, "tables", file)));
 
 	for(let table of tablesExpected) {
-		if(!~tableList.indexOf(table)) await r.tableCreate(table.name, { primary: table.primary }).run();
+		if(!~tableList.indexOf(table.name)) await r.tableCreate(table.name, { primaryKey: table.primary }).run();
 
 		const indexList = await r.table(table.name).indexList().run();
 		for(let index of table.indexes.filter(ind => !~indexList.indexOf(ind))) {
