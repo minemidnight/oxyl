27c8e152dfb6a5b52ef5765dd4eb98e13575c8d8
minemidnight <minemidnightmc@gmail.com>
2017-10-09 17:51:24 -0400

    Use path module for fs

--- src/rethinkdb/setup.js
+++ src/rethinkdb/setup.js
@@ -1,12 +1,14 @@
 const { readdir } = require("fs");
 const { promisify } = require("util");
 const readdirAsync = promisify(readdir);
+const path = require("path");
 
 module.exports = async r => { // eslint-disable-line id-length
 	const tableList = await r.tableList().run();
-	const tableExpected = (await readdirAsync("./tables")).map(file => require(`./tables/${file}`));
+	const tablesExpected = (await readdirAsync(path.resolve(__dirname, "tables")))
+		.map(file => require(path.resolve(__dirname, "tables", file)));
 
-	for(let table of tableExpected) {
+	for(let table of tablesExpected) {
 		if(!~tableList.indexOf(table)) await r.tableCreate(table.name, { primary: table.primary }).run();
 
 		const indexList = await r.table(table.name).indexList().run();
